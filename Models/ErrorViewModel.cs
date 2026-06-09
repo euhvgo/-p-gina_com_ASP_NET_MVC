be6dd36c8500f15645bb919e0d799d39b9b665b7
euhvgo <vv.hugo@uni9.edu.br>
2026-06-09 19:19:35 +0000

    Add student profile MVC pages

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _p_gina_com_ASP_NET_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
